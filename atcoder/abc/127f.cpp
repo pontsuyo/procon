c293d039dc1de9e5d56221374526043fdb2fd644
pontsuyo <tsuyopon9221@gmail.com>
2019-05-26 01:04:18 +0900

    modify template

--- atcoder/abc/127f.cpp
+++ atcoder/abc/127f.cpp
@@ -4,5 +4,8 @@ typedef long long ll;
 #define rep(i,n) for(int i=0;i<n;i++)
 
 int main(){
+    int n;
+    cin >> n;
+    printf("%d\n", n);
     return 0;
 }
--- atcoder/abc/template.cpp
+++ atcoder/abc/template.cpp
@@ -4,5 +4,8 @@ typedef long long ll;
 #define rep(i,n) for(int i=0;i<n;i++)
 
 int main(){
+    int N;
+    cin >> N;
+    printf("%d\n", n);
     return 0;
 }
