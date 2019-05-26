921a280d4ccbddfd6086fd75da3ba2eb954c383a
pontsuyo <tsuyopon9221@gmail.com>
2019-05-26 17:01:56 +0900

    127e_ans

---
+++ atcoder/abc/127e_ans.cpp
@@ -0,0 +1,34 @@
+#include<cstdio>
+#include<iostream>
+using namespace std;
+typedef long long LL;
+const int mod=1000000007;
+int mul(int x,int k)
+{
+	LL ans=1,mid=x;
+	while(k)
+	{
+		if(k&1) ans=ans*mid%mod;
+		mid=mid*mid%mod;
+		k>>=1;
+	}
+	return ans;
+}
+int main()
+{
+	int N,M,K;
+    LL ans1=1,ans2=1;
+    scanf("%d%d%d",&N,&M,&K);
+	int u=N*M;
+    N+=M,M=u;
+	
+    for(int i=0;i<K-2;++i)
+	{
+		ans1=ans1*(i+1)%mod;
+		ans2=ans2*(M-2-i)%mod;
+	}
+	ans2=ans2*mul(ans1,mod-2)%mod;
+	ans2=ans2*mul(6,mod-2)%mod*M%mod*N%mod*(M-1)%mod;
+	printf("%lld\n",ans2);
+	return 0;
+}
