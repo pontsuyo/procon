7633d549717cac29fa01208a59b3314ec7f3fa2e
pontsuyo <tsuyopon9221@gmail.com>
2019-06-13 00:52:42 +0900

    seiri

--- ari/shakutori.cpp
+++ ari/shakutori.cpp
@@ -8,24 +8,6 @@ int n, S;
 int a[MAX_N];
 int sum[MAX_N + 1];
 
-// void solve(){
-//     rep(i, n){
-//         sum[i+1] = sum[i] + a[i];
-//     }
-
-//     if(sum[n]<S){
-//         printf("0\n");
-//         return;
-//     }
-
-//     int res = n;
-//     for(int s=0; sum[s] + S <= sum[n];s++){
-//         int t = lower_bound(sum+s, sum+n, sum[s]+S) - sum;
-//         res = min(res, t-s);
-//     }
-//     printf("%d\n", res);
-// }
-
 void solve(){
     int res = n+1;
     int s=0, t=0, sum=0;
