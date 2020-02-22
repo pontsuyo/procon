708cef33999d97ed5f5b869600626892295ffe33
pontsuyo <tsuyopon9221@gmail.com>
2020-02-22 22:41:23 +0900

    0222

---
+++ atcoder/abc/156b.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define chmin(x, y) x = min(x, y)
+#define chmax(x, y) x = max(x, y)
+#define MOD (int) (1e9+7)
+#define INF (int) 2e9
+#define LLINF (ll) 2e18
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    int prod = 1;
+    int cnt=0;
+    while(true){
+        prod *= k;
+        cnt++;
+        if(prod>n) break;
+    }
+    
+    cout << cnt << endl;
+    // printf("%d\n", N);
+    return 0;
+}
---
+++ atcoder/abc/156d.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define chmin(x, y) x = min(x, y)
+#define chmax(x, y) x = max(x, y)
+#define MOD (int) (1e9+7)
+#define INF (int) 2e9
+#define LLINF (ll) 2e18
+
+ll modpow(ll a, ll n) {
+    ll res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * a % MOD;
+        a = a * a % MOD;
+        n >>= 1;
+    }
+    return res;
+}
+
+ll modfact(ll n){
+    ll ans = 1;
+    for (ll i = 1; i <= n; i++){   
+        ans *= i;
+        ans %= MOD;
+    }
+    return ans;
+}
+
+ll modcomb(ll n, ll k){
+    ll tmp = modfact(n);
+    tmp *= modpow(modfact(n-k), MOD-2);
+    tmp %= MOD;
+    tmp *= modpow(modfact(k), MOD-2);
+    tmp %= MOD;
+    return tmp;
+}
+
+ll modcomb2(ll n, ll k){
+    ll ans = 1;
+    for (ll i = (n-k+1); i <= n; i++){   
+        ans *= i;
+        ans %= MOD;
+    }
+    for (ll i = 1; i <= k; i++){   
+        ans *= modpow(i, MOD-2);
+        ans %= MOD;
+    }
+    return ans;
+}
+
+int main(){
+    ll n, a, b;
+    cin >> n >> a >> b;
+    ll ans = modpow(2, n);
+    ll aa = modcomb2(n, a);
+    ll bb = modcomb2(n, b);
+    ll tmp = ans - aa -bb -1;
+    while(tmp<0){
+        tmp += MOD;
+    }
+    cout << tmp << endl;
+    // printf("%d\n", N);
+    return 0;
+}
