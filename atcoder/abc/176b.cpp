f7955fbbd8af105825ec5995f0b44dac108b261c
pontsuyo <tsuyopon9221@gmail.com>
2020-10-11 12:39:08 +0900

    201011

---
+++ atcoder/abc/176b.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repr(i, l, r) for (int i = l; i <= (int)(r); i++)
+#define chmin(x, y) x = min(x, y)
+#define chmax(x, y) x = max(x, y)
+#define all(v) v.begin(), v.end()
+#define MOD (int) (1e9+7)
+#define INF (int) 1e9
+#define LLINF (ll) 1e18
+
+int main(){
+    string s;
+    cin >> s;
+
+    int sum = 0;
+    rep(i, s.size()){
+        sum += (int)(s[i]-'0');
+    }
+
+    if(sum%9==0){
+        cout << "Yes" << endl;
+        return 0;
+    }
+    cout << "No" << endl;
+    // printf("%d\n", N);
+    return 0;
+}
