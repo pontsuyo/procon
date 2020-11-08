ba1ea5d24053813dcef5fe7ba8e91d5a86b874ed
pontsuyo <tsuyopon9221@gmail.com>
2020-11-08 23:25:01 +0900

    201108

---
+++ atcoder/abc/82a.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repr(i, l, r) for (int i = l; i <= (int)(r); i++)
+#define chmin(x, y) x = min(x, y)
+#define chmax(x, y) x = max(x, y)
+#define all(v) v.begin(), v.end()
+#define MOD (int) (1e9+7)
+#define INF (int) 1e9
+#define LLINF (ll) 1e18
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << (a+b+1)/2 << endl;
+    // printf("%d\n", N);
+    return 0;
+}
