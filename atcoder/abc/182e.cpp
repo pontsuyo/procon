23aa4de684abda80264f01a65c80a20bf62182f0
pontsuyo <tsuyopon9221@gmail.com>
2021-03-03 00:48:07 +0900

    20210303

--- atcoder/abc/182e.cpp
+++ atcoder/abc/182e.cpp
@@ -11,27 +11,65 @@ typedef pair<int, int> P;
 #define INF (int) 1e9
 #define LLINF (ll) 1e18
 
+int t[1505][1505];
+int v[1505][1505];
+
 int main(){
     int h, w, n, m;
     cin >> h >> w >> n >> m;
 
     int a[n], b[n], c[m], d[m];
 
-    rep(i, n) cin >> a[i] >> b[i];
-    rep(i, n) a[i]--, b[i]--;
-    rep(i, m) cin >> c[i] >> d[i];
-    rep(i, m) c[i]--, d[i]--;
-
-
-    bool tate[h][w], yoko[h][w];
-    rep(hi, h) rep(wi, w) tate[hi][wi] = false;
-    rep(hi, h) rep(wi, w) yoko[hi][wi] = false;
-
-    rep(ni, n){
-        
+    rep(i, n){
+        cin >> a[i] >> b[i];
+        a[i]--, b[i]--;
+        t[a[i]][b[i]] = 1;
     }
 
-    cout << n << endl;
+    rep(i, m){
+        cin >> c[i] >> d[i];
+        c[i]--, d[i]--;
+        t[c[i]][d[i]] = 2;
+    }
+
+    rep(i, n){
+        int ti = a[i];
+        int tj = b[i];
+        while(tj+1<w && t[ti][tj+1]!=2){
+            // if(t[ti][tj+1]==1) break;
+            v[ti][tj+1] = 1;
+            tj++;
+        }
+
+        ti = a[i];
+        tj = b[i];
+        while(tj-1>=0 && t[ti][tj-1]!=2){
+            // if(t[ti][tj-1]==1) break;
+            v[ti][tj-1] = 1;
+            tj--;
+        }
+
+        ti = a[i];
+        tj = b[i];
+        while(ti+1<h && t[ti+1][tj]!=2){
+            // if(t[ti+1][tj]==1) break;
+            v[ti+1][tj] = 1;
+            ti++;
+        }
+
+        ti = a[i];
+        tj = b[i];
+        while(ti-1>=0 && t[ti-1][tj]!=2){
+            // if(t[ti-1][tj]==1) break;
+            v[ti-1][tj] = 1;
+            ti--;
+        }
+    }
+
+    int ans = 0;
+    rep(i, h) rep(j, w) if(v[i][j]==1) ans++;
+
+    cout << ans+n << endl;
     // printf("%d\n", N);
     return 0;
 }
